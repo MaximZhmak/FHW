32051f899a157cf68e4c8c94e7bdd2819e184d76
Maxim Zhmak <maximzhmak@gmail.com>
2017-01-13 16:52:15 +0200

    modified

--- 5/22.cpp
+++ 5/22.cpp
@@ -8,147 +8,159 @@ using namespace std;
 
 class String
 {
-	char* m_buffer;
-	size_t* m_refCounter;
+    char* m_buffer;
+    size_t* m_refCounter;
 
-	void Attach(const String& rhs)
-	{
+    void Attach(const String& rhs)
+    {
 
-		m_refCounter = rhs.m_refCounter;
-		m_buffer = rhs.m_buffer;
-		(*m_refCounter)++;
-	}
-	void Detach()
-	{
-		(*m_refCounter)--;
-
-		m_buffer = nullptr;
-
-	}
+        m_refCounter = rhs.m_refCounter;
+        m_buffer = rhs.m_buffer;
+        (*m_refCounter)++;
+    }
+    void Detach()
+    {
+        (*m_refCounter)--;
+        if( (*m_refCounter)==0)
+        {
+               delete[]m_buffer;
+        }
+        m_buffer = nullptr;
+    }
 public:
-	String(const char* value)
-		: m_buffer(new char[strlen(value) + 1]),
-		m_refCounter(new size_t(0))
-	{
-		strcpy(m_buffer, value);
-		(*m_refCounter)++;
-	}
+    String(const char* value)
+        : m_buffer(new char[strlen(value) + 1]),
+        m_refCounter(new size_t(0))
+    {
+        strcpy(m_buffer, value);
+        (*m_refCounter)++;
+    }
 
-	String(const String& rhs)
-		: m_buffer(rhs.m_buffer)
-		, m_refCounter(rhs.m_refCounter)
-	{
-		(*m_refCounter)++;
-	}
+    String(const String& rhs)
+        : m_buffer(rhs.m_buffer)
+        , m_refCounter(rhs.m_refCounter)
+    {
+        (*m_refCounter)++;
+    }
 
-	~String()
-	{
-		(*m_refCounter)--;
-
-		if (*m_refCounter == 0)
-		{
-			if (m_buffer)
-			{
-				delete[] m_buffer;
-				m_buffer = nullptr;
-			}
-		}
-	}
+    ~String()
+    {
+        Detach();
+    }
 
 
-	String& operator=(const String& rhs)
-	{
-		if (rhs.m_buffer != m_buffer)
-		{
-			Detach();
-			Attach(rhs);
-		}
-		return *this;
-	}
+    String& operator=(const String& rhs)
+    {
 
-	String& operator=(const char* value)
-	{
-		char* temp = new char[strlen(value) + 1]();
-		strcpy(temp, value);
+        if (rhs.m_buffer != m_buffer
+                && GetCount()>1)
+        {
+            Detach();
+            Attach(rhs);
+        }
+        else
+        {
+            m_buffer=rhs.GetBuffer();
+        }
+        return *this;
+    }
 
-		Detach();
+    String& operator=(const char* value)
+    {
+        if (GetCount()>1)
+        {
+            Detach();
+        }
+            delete[]m_buffer;
+            m_buffer = new char[strlen(value) + 1];
+            strcpy(m_buffer, value);
 
-		delete[]m_buffer;
-		m_buffer = new char[strlen(temp) + 1];
-		strcpy(m_buffer, temp);
+            return *this;
+    }
+    const char& operator[](const size_t index) const
+    {
+        return m_buffer[index];
+    }
 
 
-		delete[]temp;
+    size_t GetCount() const
+    {
+        return *m_refCounter;
+    }
+    void SetElem(size_t index, char value)
+    {
 
-		return *this;
-	}
-	const char& operator[](const size_t index) const
-	{
-		return m_buffer[index];
-	}
+        if (GetCount()>1)
+      {
+            char* temp = new char[strlen(m_buffer) + 1]();
+            strcpy(temp, m_buffer);
+            temp[index] = value;
 
+            Detach();
 
-	size_t GetCount() const
-	{
-		return *m_refCounter;
-	}
-	void SetElem(size_t index, char value)
-	{
+            delete[]m_buffer;
+            m_buffer = new char[strlen(temp) + 1];
+            strcpy(m_buffer, temp);
 
-		char* temp = new char[strlen(m_buffer) + 1]();
-		strcpy(temp, m_buffer);
-		temp[index] = value;
+            delete[]temp;
+        }
+        else
+        {
+            m_buffer[index]=value;
+        }
+    }
 
-		Detach();
-
-		delete[]m_buffer;
-		m_buffer = new char[strlen(temp) + 1];
-		strcpy(m_buffer, temp);
-
-		delete[]temp;
-	}
-	
-	char* GetBuffer() const
-	{
-		return m_buffer;
-	}
+    char* GetBuffer() const
+    {
+        return m_buffer;
+    }
 };
 
 
 
-int main(int argc, char *argv[])
+int main()
 {
 
-	String s("abc");
-	assert(s.GetCount() == 1);
+    String s("abc");
+    assert(s.GetCount() == 1);
 
-	{
-		String s2 = s;
-		assert(s.GetCount() == 2);
-		assert(s2.GetCount() == 2);
-	}
+    {
+        String s2 = s;
+        assert(s.GetCount() == 2);
+        assert(s2.GetCount() == 2);
+    }
 
-	assert(s.GetCount() == 1);
+    assert(s.GetCount() == 1);
 
-	String s3 = s;
-	assert(s.GetCount() == 2);
+    String s3 = s;
+    assert(s.GetCount() == 2);
 
-	String s4("xyz");
-	s3 = s4;
+    String s4("xyz");
+    s3 = s4;
 
-	assert(s.GetCount() == 1);
-	
-	s4.SetElem(0, '@');
+    assert(s.GetCount() == 1);
 
-	assert(s3[0] == 'x');
-	assert(s4[0] == '@');
+    s4.SetElem(0, '@');
 
-
-	assert(s3.GetCount() == s4.GetCount() && s4.GetCount() == 1);
-
-	assert(strcmp(s.GetBuffer(), "abc") == 0);
+    assert(s3[0] == 'x');
+    assert(s4[0] == '@');
 
 
-	cout << "PASSED" << endl;
-	return 0;
+    assert(s3.GetCount() == s4.GetCount() && s4.GetCount() == 1);
+
+    assert(strcmp(s.GetBuffer(), "abc") == 0);
+
+    {
+                  String s1("abc");
+                  s1.SetElem(0, 'x');
+                  assert(s1.GetCount() == 1);
+    }
+
+    {
+             String s1 = "abc";
+             s1 = "xyz";
+             assert(s1.GetCount() == 1);
+     }
+    cout << "PASSED" << endl;
+    return 0;
 }
